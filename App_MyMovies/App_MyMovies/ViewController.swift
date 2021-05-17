0ceb12eb68b7b5380e9dee081dc342dea49b380b
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-05-16 22:15:57 -0300

    First Commit Project

---
+++ App_MyMovies/App_MyMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App_MyMovies
+//
+//  Created by Kaique Lopes on 16/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
