3af7d5700392a999a62536ec6fc9c522f3002e6f
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-05-27 16:10:26 -0300

    descriptionLabel

--- App_MyMovies/App_MyMovies/MovieCell.swift
+++ App_MyMovies/App_MyMovies/MovieCell.swift
@@ -10,5 +10,7 @@ import UIKit
 
 class MovieCell: UITableViewCell {
     @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
     
 }
