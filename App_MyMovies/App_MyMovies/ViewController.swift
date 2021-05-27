63b9851bf24902f2e720938a040f526f955988f9
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-05-26 22:17:39 -0300

    variables cell

--- App_MyMovies/App_MyMovies/ViewController.swift
+++ App_MyMovies/App_MyMovies/ViewController.swift
@@ -27,5 +27,9 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movies.count
     }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let movie: Movie = movies [indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: <#T##String#>, for: <#T##IndexPath#>)
+    }
 }
 
