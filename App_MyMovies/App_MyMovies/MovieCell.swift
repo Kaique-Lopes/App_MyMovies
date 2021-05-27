61a2c2775596b7b3b4e4f507d716c13be5162257
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-05-27 15:53:09 -0300

    MovieCell

---
+++ App_MyMovies/App_MyMovies/MovieCell.swift
@@ -0,0 +1,8 @@
+//
+//  MovieCell.swift
+//  App_MyMovies
+//
+//  Created by Kaique Lopes on 27/05/21.
+//
+
+import Foundation
--- App_MyMovies/App_MyMovies/ViewController.swift
+++ App_MyMovies/App_MyMovies/ViewController.swift
@@ -20,6 +20,30 @@ class ViewController: UITableViewController {
         
         movie = Movie(title: "Filme 2", description: "Descrição Filme 2", image: UIImage(named: "filme2")!)
         movies.append(movie)
+        
+        movie = Movie(title: "Filme 3", description: "Descrição Filme 3", image: UIImage(named: "filme3")!)
+        movies.append(movie)
+        
+        movie = Movie(title: "Filme 4", description: "Descrição Filme 4", image: UIImage(named: "filme4")!)
+        movies.append(movie)
+        
+        movie = Movie(title: "Filme 5", description: "Descrição Filme 5", image: UIImage(named: "filme5")!)
+        movies.append(movie)
+        
+        movie = Movie(title: "Filme 6", description: "Descrição Filme 6", image: UIImage(named: "filme6")!)
+        movies.append(movie)
+        
+        movie = Movie(title: "Filme 7", description: "Descrição Filme 7", image: UIImage(named: "filme7")!)
+        movies.append(movie)
+        
+        movie = Movie(title: "Filme 8", description: "Descrição Filme 8", image: UIImage(named: "filme8")!)
+        movies.append(movie)
+        
+        movie = Movie(title: "Filme 9", description: "Descrição Filme 9", image: UIImage(named: "filme9")!)
+        movies.append(movie)
+        
+        movie = Movie(title: "Filme 10", description: "Descrição Filme 10", image: UIImage(named: "filme10")!)
+        movies.append(movie)
     }
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
