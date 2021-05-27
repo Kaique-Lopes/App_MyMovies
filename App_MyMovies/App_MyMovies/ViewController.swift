9be0fba6807ea2f66a26db81f24cc45b9086892e
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-05-26 22:14:13 -0300

    numberOfRowsInSection

--- App_MyMovies/App_MyMovies/ViewController.swift
+++ App_MyMovies/App_MyMovies/ViewController.swift
@@ -24,6 +24,8 @@ class ViewController: UITableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
 }
 
