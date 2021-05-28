4fa610f3c4b89ceddf6c1e05cbb8ab15cec084ad
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-05-27 22:08:28 -0300

    class DetailsMovieViewController

---
+++ App_MyMovies/App_MyMovies/DetailsMovieViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailsMovieViewController.swift
+//  App_MyMovies
+//
+//  Created by Kaique Lopes on 27/05/21.
+//
+
+import Foundation
+import UIKit
+
+class DetailsMovieViewController: UIViewController {
+    
+}
